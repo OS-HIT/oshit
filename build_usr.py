9f9f12b92df53839ddca7cf055b7ee45a6a625b0
willson0v0 <willsonlgz@gmail.com>
2021-03-15 18:16:52 +0800

    Add script to auto link user programs

---
+++ build_usr.py
@@ -0,0 +1,29 @@
+import os
+
+base_address = 0x80400000
+step = 0x20000
+linker = 'user_linker.ld'
+
+app_id = 0
+apps = os.listdir('.')
+apps.sort()
+lines_before = []
+with open(linker, 'r') as f:
+    for line in f.readlines():
+        lines_before.append(line)
+
+for app in apps:
+    if app[:4] != "usr_":
+        continue 
+    lines = []
+    for line in lines_before:
+        line_altered = line.replace(hex(base_address), hex(base_address+step*app_id))
+        lines.append(line_altered)
+    with open(app + "/src/" + linker, 'w+') as f:
+        f.writelines(lines)
+    os.chdir(app)
+    os.system('cargo build --bin %s --release' % app)
+    os.system("rust-objcopy --binary-architecture=riscv64 target/riscv64gc-unknown-none-elf/release/%s --strip-all -O binary ../user_bins/%s.bin" % (app, app))
+    os.chdir("..")
+    print('[build.py] application %s start with address %s' %(app, hex(base_address+step*app_id)))
+    app_id = app_id + 1
--- usr_hello_world_a/src/main.rs
+++ usr_hello_world_a/src/main.rs
@@ -6,12 +6,12 @@ extern crate oshit_usrlib;
 
 use oshit_usrlib::sys_yield;
 
-const HEIGHT: usize = 5;
+const HEIGHT: usize = 2;
 
 #[no_mangle]
 fn main() -> i32 {
     for i in 0..HEIGHT {
-        print!("测试A，输出一定长度的字符串。");
+        print!("测试A，重复输出一个一定长度的字符串。");
         println!(" [{}/{}]", i + 1, HEIGHT);
         sys_yield();
     }
