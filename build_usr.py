e140a60b8601a81be98a25b0881ffa46e64c8c28
willson0v0 <willsonlgz@gmail.com>
2021-04-06 04:32:41 +0800

    Memory managment working on qemu+opensbi

--- build_usr.py
+++ build_usr.py
@@ -18,15 +18,16 @@ if not os.path.exists("user_bins"):
 for app in apps:
     if app[:4] != "usr_":
         continue 
-    lines = []
-    for line in lines_before:
-        line_altered = line.replace(hex(base_address), hex(base_address+step*app_id))
-        lines.append(line_altered)
+    lines = lines_before
+    # for line in lines_before:
+        # line_altered = line.replace(hex(base_address), hex(base_address+step*app_id))
+        # lines.append(line_altered)
     with open(app + "/src/" + linker, 'w+') as f:
         f.writelines(lines)
     os.chdir(app)
     os.system('cargo build --bin %s --release' % app)
-    os.system("rust-objcopy --binary-architecture=riscv64 target/riscv64gc-unknown-none-elf/release/%s --strip-all -O binary ../user_bins/%s.bin" % (app, app))
+    os.system("cp target/riscv64gc-unknown-none-elf/release/%s ../user_bins/%s" % (app, app))
+    # os.system("rust-objcopy --binary-architecture=riscv64 target/riscv64gc-unknown-none-elf/release/%s --strip-all -O binary ../user_bins/%s.bin" % (app, app))
     os.chdir("..")
     print('[build.py] application %s start with address %s' %(app, hex(base_address+step*app_id)))
     app_id = app_id + 1
