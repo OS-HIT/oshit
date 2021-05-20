6a6a0308f32a882b749e4368a11136311bd04856
willson0v0 <willsonlgz@gmail.com>
2021-05-20 11:08:24 +0800

    adding execve of fs support

--- user_programs/proc0/src/main.rs
+++ user_programs/proc0/src/main.rs
@@ -8,7 +8,7 @@ extern crate oshit_usrlib;
 
 use oshit_usrlib::{
     sys_fork,
-    wait,
+    sys_waitpid,
     sys_exec,
     sys_yield,
 };
@@ -17,12 +17,12 @@ use oshit_usrlib::{
 fn main() -> i32 {
     println!("[proc0] Started.");
     if sys_fork() == 0 {
-        sys_exec(b"shell\0".as_ptr());
+        sys_exec(b"/shell\0".as_ptr());
     } else {
         loop {
             let mut exit_code: i32 = 0;
-            let pid = wait(&mut exit_code);
-            if pid == -1{
+            let pid = sys_waitpid(-1, &mut exit_code);
+            if pid == -1 || pid == -2{
                 sys_yield();
                 continue;
             } 
