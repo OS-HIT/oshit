30cbf9b63a21061b8e6fd13dd6bd0a993460ee76
willson0v0 <willsonlgz@gmail.com>
2021-05-31 00:00:47 +0800

    sync

--- user_programs/proc0/src/main.rs
+++ user_programs/proc0/src/main.rs
@@ -11,41 +11,43 @@ use oshit_usrlib::{
     sys_waitpid,
     sys_exec,
     sys_yield,
+    sys_getpid,
+    sys_exit
 };
 
 const programs : &'static [&'static [u8]] = &[
-    b"/shell\0", 
+    // b"/shell\0", 
     b"/brk\0", 
-    b"/chdir\0",
+    // b"/chdir\0",
     b"/clone\0",
-    b"/close\0",
-    b"/dup\0",
-    b"/dup2\0",
-    b"/execve\0",
+    // b"/close\0",
+    // b"/dup\0",
+    // b"/dup2\0",
+    // b"/execve\0",
     b"/exit\0",
     b"/fork\0",
-    b"/fstat\0",
+    // b"/fstat\0",
     b"/getcwd\0",
-    b"/getdents\0",
+    // b"/getdents\0",
     b"/getpid\0",
     b"/getppid\0",
     b"/gettimeofday\0",
-    b"/mkdir_\0",
-    b"/mmap\0",
-    b"/mount\0",
-    b"/munmap\0",
-    b"/open\0",
-    b"/openat\0",
+    // b"/mkdir_\0",
+    // b"/mmap\0",
+    // b"/mount\0",
+    // b"/munmap\0",
+    // b"/open\0",
+    // b"/openat\0",
     b"/pipe\0",
-    b"/read\0",
+    // b"/read\0",
     b"/sleep\0",
     b"/times\0",
-    b"/umount\0",
+    // b"/umount\0",
     b"/uname\0",
-    b"/unlink\0",
+    // b"/unlink\0",
     b"/wait\0",
     b"/waitpid\0",
-    b"/write\0",
+    // b"/write\0",
     b"/yield\0"
 ];
 
@@ -54,10 +56,13 @@ fn main() -> i32 {
     println!("[proc0] Started.");
     if sys_fork() == 0 {
         for prog in programs {
-            println!("Now execute: {}", core::str::from_utf8(prog).unwrap());
+            println!("This is {}, Now execute: {}", sys_getpid(), core::str::from_utf8(prog).unwrap());
             let child = sys_fork();
             if child == 0 {
-                sys_exec(prog.as_ptr(), &[0 as *const u8], &[0 as *const u8]);
+                if sys_exec(prog.as_ptr(), &[0 as *const u8], &[0 as *const u8]) == -1 {
+                    println!("Exec failed. dying.");
+                    sys_exit(-1);
+                }
             } else {
                 let mut exit_code: i32 = 0;
                 sys_waitpid(child, &mut exit_code);
